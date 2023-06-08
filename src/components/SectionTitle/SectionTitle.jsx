6079949d528ae2244a7a3424504ba06edd70b720
nazmunnaharnijhum <nazmunnaharnijhum@gmail.com>
2023-06-08 12:48:10 +0600

    section tiltle component added

---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,15 @@
+
+const SectionTitle = ({heading, subHeading}) => {
+    return (
+        <div className="mx-auto text-center md:w-4/12 my-8">
+            <h3 className=" text-4xl mb-2 text-[#d4ad76]">{heading}</h3>
+        <div className="text-green-400 font-extrabold">
+            <p>_________ *** _________</p>
+            <p>--------- *** ---------</p>
+        </div>
+            <p>{subHeading}</p>
+        </div>
+    );
+};
+
+export default SectionTitle;
---
+++ src/pages/Home/PopularClasses/PopularClasses.jsx
@@ -0,0 +1,15 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const PopularClasses = () => {
+    return (
+        <section>
+            <SectionTitle
+            heading="Our Popular Yoga Pack"
+            subHeading="We at ZenZone Camp provide various services to the nature of the clients. Wish how you would like to spend the time here
+            we can talk and come to a conclusion."
+            ></SectionTitle>
+        </section>
+    );
+};
+
+export default PopularClasses;
