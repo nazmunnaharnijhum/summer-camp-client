a19e91069494aad0e69e8d05bfe88789becc32d9
nazmunnaharnijhum <nazmunnaharnijhum@gmail.com>
2023-06-08 10:20:53 +0600

    added a footer and a navbar

---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/no-unknown-property */
+
+const Footer = () => {
+    return (
+        <footer className="mb-0">
+            <div className="footer p-10 font-semibold text-lg bg-[#d4ad76] text-black">
+  <div>
+    <img className="w-[200px] h-25" src="https://i.ibb.co/sWnt98n/360-F-389306425-z-Asi-EURL3pnv-NC3g-Pot-VFrz5wh-Xz-Msi-B-removebg-preview.png" alt="" />
+    <p>ZenZone Camp<br/>Providing reliable tech since 1992</p>
+  </div> 
+  <div>
+    <span className="footer-title">Services</span> 
+    <a className="link link-hover">Branding</a> 
+    <a className="link link-hover">Design</a> 
+    <a className="link link-hover">Marketing</a> 
+    <a className="link link-hover">Advertisement</a>
+  </div> 
+  <div>
+    <span className="footer-title">Company</span> 
+    <a className="link link-hover">About us</a> 
+    <a className="link link-hover">Contact</a> 
+    <a className="link link-hover">Jobs</a> 
+    <a className="link link-hover">Press kit</a>
+  </div> 
+  <div>
+    <span className="footer-title">Legal</span> 
+    <a className="link link-hover">Terms of use</a> 
+    <a className="link link-hover">Privacy policy</a> 
+    <a className="link link-hover">Cookie policy</a>
+  </div>
+</div>
+<div className="footer footer-center p-4 text-base-content bg-amber-900">
+  <div>
+    <img src="" alt="" />
+    <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+  </div>
+</div>
+        </footer>
+    );
+};
+
+export default Footer;
