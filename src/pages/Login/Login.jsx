6d548bcbba1bcf3b807bfe4715d3a557c84b59b9
nazmunnaharnijhum <nazmunnaharnijhum@gmail.com>
2023-06-09 18:09:06 +0600

    firebase config has been hidden

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -10,8 +10,8 @@ const Login = () => {
         <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <h1 className="text-5xl font-bold text-[#e79e37]">Login now!</h1>
+            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut <br /> assumenda excepturi exercitationem quasi. In <br /> deleniti eaque aut repudiandae et a id nisi.</p>
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <form onSubmit={handleSubmit(onSubmit)} className="card-body">
@@ -33,7 +33,7 @@ const Login = () => {
                 </label>
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
+                <button className="btn bg-[#e79e37]">Login</button>
               </div>
             </form>
           </div>
