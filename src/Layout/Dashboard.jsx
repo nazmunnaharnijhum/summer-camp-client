87affab3cd4888f4b1051fb5c015886091fddbbe
nazmunnaharnijhum <nazmunnaharnijhum@gmail.com>
2023-06-11 09:49:22 +0600

    created user

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,7 +1,9 @@
-import { FaCalendarAlt, FaHome, FaShoppingCart, FaWallet } from "react-icons/fa";
+import { FaCalendarAlt, FaHome,  FaPeopleArrows,  FaShoppingCart, FaWallet } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
+    const [cart] = useCart();
     return (
         <div className="drawer lg:drawer-open ">
   <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -15,12 +17,16 @@ const Dashboard = () => {
     <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
     <ul className="menu p-4 w-80 h-full">
       {/* Sidebar content here */}
-      <li><NavLink to="/dashboard/mycart"><FaShoppingCart></FaShoppingCart> My Selected Classes</NavLink></li>
+      <li>
+        <NavLink to="/dashboard/mycart"><FaShoppingCart></FaShoppingCart> My Selected Classes<span className="badge bg-pink-300">+{cart?.length || 0}</span>
+        </NavLink>
+        
+        </li>
       <li><NavLink to="/dashboard/enrolled"><FaCalendarAlt></FaCalendarAlt> My Enrolled Classes</NavLink></li>
       <li><NavLink to="/dashboard/myhistory"><FaWallet></FaWallet>Payment History</NavLink></li>
       <div className="divider"></div>
       <li><NavLink to="/"><FaHome></FaHome>Home</NavLink></li>
-      <li><NavLink> Classes</NavLink></li>
+      <li><NavLink to="/classes"><FaPeopleArrows></FaPeopleArrows> Classes</NavLink></li>
       <li></li>
     </ul>
   
